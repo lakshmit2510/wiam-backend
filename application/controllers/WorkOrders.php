26b016c68bcf2a8467d20c433f923a22a50be66d
yedukondalu vanipenta <yedukondalu.vanipenta@xyleminc.com>
2019-11-13 00:37:03 +0800

    CRUD apis for work orders

--- application/controllers/WorkOrders.php
+++ application/controllers/WorkOrders.php
@@ -59,6 +59,14 @@ class WorkOrders extends REST_Controller
 
         $this->response(['Delete working'], REST_Controller::HTTP_OK);
     }
+    public function getAllWorkOrders_get()
+    {
+
+        $work_orders_list = $this->WorkOrders_Model->getAllWorkOrders();
+
+        $this->response($work_orders_list, REST_Controller::HTTP_OK);
+
+    }
     public function getWorkOrdersById_get()
     {
         $work_order_id = $this->get('work_order_id');
@@ -66,6 +74,58 @@ class WorkOrders extends REST_Controller
         $work_orders_list = $this->WorkOrders_Model->getWorkOrdersById($work_order_id);
 
         $this->response($work_orders_list, REST_Controller::HTTP_OK);
+
+    }
+
+    public function createWorkOrder_post()
+    {
+        $db_values = array();
+        $db_values['Code'] = $this->post('Code');
+        $db_values['Priority'] = $this->post('Priority');
+        $db_values['AssetCode'] = $this->post('AssetCode');
+        $db_values['PartsID'] = $this->post('PartsID');
+        $db_values['AssignedUsers'] = $this->post('AssignedUsers');
+        $db_values['WorkType'] = $this->post('WorkType');
+        $db_values['CompletedByUsers'] = $this->post('CompletedByUsers');
+        $db_values['EstimatedHours'] = $this->post('EstimatedHours');
+        $db_values['SpentHours'] = $this->post('SpentHours');
+        $db_values['SummaryIssue'] = $this->post('SummaryIssue');
+        $db_values['RequestedCompletionDate'] = $this->post('RequestedCompletionDate');
+        $db_values['Status'] = $this->post('Status');
+
+        $this->WorkOrders_Model->createWorkOrder($db_values);
+
+        $this->response(["Work Order Created Successfully"], REST_Controller::HTTP_OK);
+
+    }
+    public function updateWorkOrdersById_put()
+    {
+        $db_values = array();
+        $ID = $this->put('ID');
+        $db_values['Code'] = $this->put('Code');
+        $db_values['Priority'] = $this->put('Priority');
+        $db_values['AssetCode'] = $this->put('AssetCode');
+        $db_values['PartsID'] = $this->put('PartsID');
+        $db_values['AssignedUsers'] = $this->put('AssignedUsers');
+        $db_values['WorkType'] = $this->put('WorkType');
+        $db_values['CompletedByUsers'] = $this->put('CompletedByUsers');
+        $db_values['EstimatedHours'] = $this->put('EstimatedHours');
+        $db_values['SpentHours'] = $this->put('SpentHours');
+        $db_values['SummaryIssue'] = $this->put('SummaryIssue');
+        $db_values['RequestedCompletionDate'] = $this->put('RequestedCompletionDate');
+        $db_values['Status'] = $this->put('Status');
+
+        $this->WorkOrders_Model->updateWorkOrdersById($ID, $db_values);
+
+        $this->response(["Work Order Updated Successfully"], REST_Controller::HTTP_OK);
+
+    }
+    public function deleteWorkOrdersById_delete($work_order_id)
+    {
+
+        $this->WorkOrders_Model->deleteWorkOrdersById($work_order_id);
+
+        $this->response(["Work Order Deleted Successfully"], REST_Controller::HTTP_OK);
 
     }
 }
--- application/models/WorkOrders_Model.php
+++ application/models/WorkOrders_Model.php
@@ -3,14 +3,40 @@
 class WorkOrders_Model extends CI_Model
 {
 
+    public function getAllWorkOrders()
+    {
+        $query = $this->db->get('workorders');
+
+        return $query->result();
+    }
+
     public function getWorkOrdersById($work_order_id)
     {
-        // $query = $this->db->get('workorders');
-        // $this->db->where("ID", $work_order_id);
 
         $query = $this->db->get_where('workorders', array('ID' => $work_order_id));
 
         return $query->result();
+    }
+
+    public function updateWorkOrdersById($work_order_id, $data)
+    {
+
+        $this->db->where('ID', $work_order_id);
+        $this->db->update('workorders', $data);
+
+    }
+
+    public function createWorkOrder($data)
+    {
+
+        $this->db->insert('workorders', $data);
+
+    }
+    public function deleteWorkOrdersById($work_order_id)
+    {
+
+        $this->db->delete('workorders', array('ID' => $work_order_id));
+
     }
 
 }
